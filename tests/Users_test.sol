0a8626bd2a53a3cbf5f76afdc2dff3ea5eff8ce7
AstroSamus <adrian.sequeira@utonoma.com>
2023-11-17 00:41:26 -0600

    Refactoring the Users_test file to host test that depends on the msg.sender and adding tests for the MAU calculation with multiple accounts. To be able to change correctly the msg.sender is necesary to inherit from the contract that will be tested instead of creating an instance to call the methods to test, as can be seen in this post: https://github.com/ethereum/remix-project/issues/1618

--- tests/Users_test.sol
+++ tests/Users_test.sol
@@ -7,14 +7,13 @@ import "remix_accounts.sol";
 import "../contracts/Users.sol";
 import {Comparators} from "tests/test_utils/Comparators.sol";
 
-contract Users_test is Comparators {
+contract Users_test is Users, Comparators {
 
-    Users usersContract;
     uint256 startTime = 1673762400; //Sun Jan 15 2023 06:00:00 GMT+0000
+    uint256 currentTime = 1673848800; //Mon Jan 16 2023 06:00:00 GMT+0000 (One day after start time)
     uint256[] MAUReport; 
 
     function beforeAll() public {
-        usersContract = new Users();
         Assert.ok(true, "true");
     }
 
@@ -31,75 +30,112 @@ contract Users_test is Comparators {
     *   Nineth period: start = 1694498400, end = 1697090399.
     */   
     /// #sender: account-1
-    function calculateMAU() public {
-        uint256 currentTime = 1673848800; //Mon Jan 16 2023 06:00:00 GMT+0000 (One day after start time)
+    function calculateMAUForASingleUser() public {
         
-        usersContract.calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
 
-        MAUReport.push(1);
+        MAUReport.push(1);//MAUReport should be [1]
         Assert.ok(
-            arrayComparator(usersContract.getAllMAUDataInOneMonthInvervals(), MAUReport),
+            arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method from an account that interacts for the fisrt time with the contract, the MAU report should reflect one user"
         );
 
         
         currentTime = 1674280800; //Sat Jan 21 2023 06:00:00 GMT+0000 (five days after the first interaction)
-        usersContract.calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
 
 
         Assert.ok(
-            arrayComparator(usersContract.getAllMAUDataInOneMonthInvervals(), MAUReport),
+            arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the second time with the same account in the current period, the MAU report should only reflect one user"
         );
         Assert.equal( 
-            usersContract.getLatestInteractionTime(TestsAccounts.getAccount(1)),
+            getLatestInteractionTime(TestsAccounts.getAccount(1)),
             currentTime,
             "When using the calculateMAU method for the second time with the same account in the current period, the latest interaction time of the user should correspond to the time of the latest call to the method"
         );
 
 
         currentTime = 1676354400; //Tue Feb 14 2023 06:00:00 GMT+0000 (at the start of the second period)
-        usersContract.calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
 
-        MAUReport.push(1);
+        MAUReport.push(1); //MAUReport should be [1,1]
         Assert.ok(
-            arrayComparator(usersContract.getAllMAUDataInOneMonthInvervals(), MAUReport),
+            arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the first time since the start of the second period from an account that interacted in the pervious period, MAU report should count one user in the starting period"
         );
 
 
         currentTime = 1681538400; //Sat Apr 15 2023 06:00:00 GMT+0000 (in the fourth period after skipping the third one)
-        usersContract.calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
 
         MAUReport.push(0);
         MAUReport.push(1); //MAUReport should be [1,1,0,1]
         Assert.ok( 
-            arrayComparator(usersContract.getAllMAUDataInOneMonthInvervals(), MAUReport),
+            arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the first time after one period with no users, the MAU report of the period with no users should be in zero and the current one should be in one"
         );
 
 
         currentTime = 1691906400; //Sun Aug 13 2023 06:00:00 GMT+0000 (Begining of the eigth period after three monts with no users)  
-        usersContract.calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
 
         MAUReport.push(0);
         MAUReport.push(0);
         MAUReport.push(0);
         MAUReport.push(1); //MAUReport should be [1,1,0,1,0,0,0,1]
         Assert.ok( 
-            arrayComparator(usersContract.getAllMAUDataInOneMonthInvervals(), MAUReport),
+            arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the first time after three periods with no users, the MAU report of the three periods with no users should be in zero and the current one should be in one"
         );
 
 
         currentTime = 1694498400; //Tue Sep 12 2023 06:00:00 GMT+0000 (Begining of the nineth period)          
-        usersContract.calculateMAU(currentTime, startTime);
-        usersContract.calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
+        calculateMAU(currentTime, startTime);
 
         MAUReport.push(1); //MAUReport should be [1,1,0,1,0,0,0,1,1]
         Assert.ok(
-            arrayComparator(usersContract.getAllMAUDataInOneMonthInvervals(), MAUReport),
-            "When using the calculateMAU method twice at the same exact time with the same account, the MAU report should only reflect one interaction and not two"
+            arrayComparator(getMAUReport(), MAUReport),
+            "When using the calculateMAU method twice at the same exact time with the same account, the MAU report should only reflect one user and not two"
+        );
+    }
+
+    /// #sender: account-2
+    function calculateMAUForTwoUsers() public {
+        currentTime = 1694498400; //Tue Sep 12 2023 06:00:00 GMT+0000 (Begining of the nineth period)
+        calculateMAU(currentTime, startTime);
+
+        MAUReport[MAUReport.length - 1]++; //MAUReport should be [1,1,0,1,0,0,0,1,2]
+        Assert.ok(
+            arrayComparator(getMAUReport(), MAUReport),
+            "When using the calculateMAU method twice in the same period from two different accounts, the MAU report should reflect two users for the current period"
+        );
+        
+
+        calculateMAU(currentTime, startTime);
+        Assert.ok(
+            arrayComparator(getMAUReport(), MAUReport),
+            "When using the calculateMAU method three times in the same period from two different accounts, the MAU report should reflect only two users for the current period and not three"
+        );
+    }
+
+    /// #sender: account-3
+    function calculateMAUForThreeUsers() public {
+        currentTime = 1694498400; //Tue Sep 12 2023 06:00:00 GMT+0000 (Begining of the nineth period)
+        calculateMAU(currentTime, startTime);
+
+        MAUReport[MAUReport.length - 1]++; //MAUReport should be [1,1,0,1,0,0,0,1,3]
+        Assert.ok(
+            arrayComparator(getMAUReport(), MAUReport),
+            "When using the calculateMAU method three times in the same period from three different accounts, the MAU report should reflect three users for the current period"
+        );
+        
+
+        calculateMAU(currentTime, startTime);
+        Assert.ok(
+            arrayComparator(getMAUReport(), MAUReport),
+            "When using the calculateMAU method multiple times in the same period from three different accounts, the MAU report should reflect only three users for the current period"
         );
     }
 }
