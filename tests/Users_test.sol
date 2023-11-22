46affe633bd5c7a190cf480bd98557df4637e252
AstroSamus <adrian.sequeira@utonoma.com>
2023-11-22 14:19:52 -0600

    Unit testing for the getMAU method. Tests for cases when no users, first month and the rest of the time

--- tests/Users_test.sol
+++ tests/Users_test.sol
@@ -13,8 +13,27 @@ contract Users_test is Users, Comparators {
     uint256 currentTime = 1673848800; //Mon Jan 16 2023 06:00:00 GMT+0000 (One day after start time)
     uint256[] MAUReport; 
 
-    function beforeAll() public {
-        Assert.ok(true, "true");
+    function getMAUForNoUsers() public {
+        Assert.equal(
+            getMAU(),
+            0,
+            "When using the getMAU method at the initialization of the smart contract, the result should be 0 users"
+        );
+    }
+
+    /// @dev Generate one interaction for the next test
+    /// #sender: account-5
+    function generateOneInteracionForTheNextTest() public {
+        calculateMAU(currentTime, startTime);
+    }
+    /// #sender: account-6
+    function getMAUForTheFirstMonth() public {
+        calculateMAU(currentTime, startTime);
+        Assert.equal(
+            getMAU(),
+            2,
+            "When using the getMAU method in the first month, the result should be number of users of the current month (that is 2)"
+        );
     }
 
     /**
@@ -34,12 +53,11 @@ contract Users_test is Users, Comparators {
         
         calculateMAU(currentTime, startTime);
 
-        MAUReport.push(1);//MAUReport should be [1]
+        MAUReport.push(3);//MAUReport should be [3] (two from the previous test and one from this)
         Assert.ok(
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method from an account that interacts for the fisrt time with the contract, the MAU report should reflect one user"
         );
-
         
         currentTime = 1674280800; //Sat Jan 21 2023 06:00:00 GMT+0000 (five days after the first interaction)
         calculateMAU(currentTime, startTime);
@@ -59,7 +77,7 @@ contract Users_test is Users, Comparators {
         currentTime = 1676354400; //Tue Feb 14 2023 06:00:00 GMT+0000 (at the start of the second period)
         calculateMAU(currentTime, startTime);
 
-        MAUReport.push(1); //MAUReport should be [1,1]
+        MAUReport.push(1); //MAUReport should be [3,1]
         Assert.ok(
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the first time since the start of the second period from an account that interacted in the pervious period, MAU report should count one user in the starting period"
@@ -70,7 +88,7 @@ contract Users_test is Users, Comparators {
         calculateMAU(currentTime, startTime);
 
         MAUReport.push(0);
-        MAUReport.push(1); //MAUReport should be [1,1,0,1]
+        MAUReport.push(1); //MAUReport should be [3,1,0,1]
         Assert.ok( 
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the first time after one period with no users, the MAU report of the period with no users should be in zero and the current one should be in one"
@@ -83,7 +101,7 @@ contract Users_test is Users, Comparators {
         MAUReport.push(0);
         MAUReport.push(0);
         MAUReport.push(0);
-        MAUReport.push(1); //MAUReport should be [1,1,0,1,0,0,0,1]
+        MAUReport.push(1); //MAUReport should be [3,1,0,1,0,0,0,1]
         Assert.ok( 
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method for the first time after three periods with no users, the MAU report of the three periods with no users should be in zero and the current one should be in one"
@@ -94,7 +112,7 @@ contract Users_test is Users, Comparators {
         calculateMAU(currentTime, startTime);
         calculateMAU(currentTime, startTime);
 
-        MAUReport.push(1); //MAUReport should be [1,1,0,1,0,0,0,1,1]
+        MAUReport.push(1); //MAUReport should be [3,1,0,1,0,0,0,1,1]
         Assert.ok(
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method twice at the same exact time with the same account, the MAU report should only reflect one user and not two"
@@ -106,7 +124,7 @@ contract Users_test is Users, Comparators {
         currentTime = 1694498400; //Tue Sep 12 2023 06:00:00 GMT+0000 (Begining of the nineth period)
         calculateMAU(currentTime, startTime);
 
-        MAUReport[MAUReport.length - 1]++; //MAUReport should be [1,1,0,1,0,0,0,1,2]
+        MAUReport[MAUReport.length - 1]++; //MAUReport should be [3,1,0,1,0,0,0,1,2]
         Assert.ok(
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method twice in the same period from two different accounts, the MAU report should reflect two users for the current period"
@@ -125,7 +143,7 @@ contract Users_test is Users, Comparators {
         currentTime = 1694498400; //Tue Sep 12 2023 06:00:00 GMT+0000 (Begining of the nineth period)
         calculateMAU(currentTime, startTime);
 
-        MAUReport[MAUReport.length - 1]++; //MAUReport should be [1,1,0,1,0,0,0,1,3]
+        MAUReport[MAUReport.length - 1]++; //MAUReport should be [3,1,0,1,0,0,0,1,3]
         Assert.ok(
             arrayComparator(getMAUReport(), MAUReport),
             "When using the calculateMAU method three times in the same period from three different accounts, the MAU report should reflect three users for the current period"
@@ -138,6 +156,18 @@ contract Users_test is Users, Comparators {
             "When using the calculateMAU method multiple times in the same period from three different accounts, the MAU report should reflect only three users for the current period"
         );
     }
+
+    function getMAUForMoreThanAMonthOfExistanceOfTheApp() public{
+        //Current time is: Tue Sep 12 2023 06:00:00 GMT+0000 (Begining of the nineth period)
+        //Current period has 3 users if we get the MAU we should get the users of the previous
+        //period (1)
+        Assert.equal(
+            getMAU(),
+            1,
+            "When using the getMAU method at the begining of the nineth period, the result should be the number of users of the eight period, that is 1"
+        );
+    }    
+    
 }
 
 
