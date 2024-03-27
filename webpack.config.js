ff4b2b26b4e59027cffa8f6f4f814f9c01b6032a
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-03-27 01:06:59 -0600

    Serving static files through the assets folder. Useful for serving images, fonts and other static files. CopyPlugin was added to webpack.config for the job.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -11,6 +12,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "assets/"),
+          to: path.resolve(__dirname, "dist/assets/"),
+        }
+      ],
     }),
   ],
   output: {
