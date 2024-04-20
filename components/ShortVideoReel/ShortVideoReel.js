66bfacbc3857069e9bc6a9a8319a3cb7cfa4c71b
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-04-19 18:42:54 -0600

    In the short video reel, we created a functionality to watch the next video when pressing the next button. For now, only gets new short videos randomly.

--- components/ShortVideoReel/ShortVideoReel.js
+++ components/ShortVideoReel/ShortVideoReel.js
@@ -1,5 +1,17 @@
-document.querySelector('#buttonNextShortVideo').addEventListener('click', () => {
-  console.log('Next short video, please!!')
+import { getShortVideo } from "../../services/contentProvider"
+import { getIpfsHashFromBytes32, getUrlFromIpfsHash } from "../../utils/encodingUtils"
+
+const $shortVideoPlayer = document.querySelector('#shortVideoPlayer')
+
+document.querySelector('#buttonNextShortVideo').addEventListener('click', async () => {
+  const { authorAddress, contentId, metadata, likes } = await getShortVideo()
+  $shortVideoPlayer.src = getUrlFromIpfsHash(getIpfsHashFromBytes32(contentId))
+  try {
+    $shortVideoPlayer.load()
+    $shortVideoPlayer.play()
+  } catch(error) {
+    console.log(error)
+  }
 })
 
 document.querySelector('#buttonPreviousShortVideo').addEventListener('click', () => {
