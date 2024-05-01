073fb4163acd3a85a6a0c198c60ca1eb35da8b52
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-05-01 13:15:03 -0600

    ShortVideoReel retries to fetch a new short video in case that the loading of previous one fails In the previous implementation, if the loading of the video fails, most probably because the content is not a video (maybe a text or an image), the browser will throw a DOMException and nothing will happens until the user press the button for getting the next video. Now, this implementation tries 5 times to get a new video in case of DOMException when playing the previous

--- components/ShortVideoReel/ShortVideoReel.js
+++ components/ShortVideoReel/ShortVideoReel.js
@@ -1,13 +1,14 @@
 import { getShortVideo } from "../../services/contentProvider.js"
 import { getIpfsHashFromBytes32, getUrlFromIpfsHash } from "../../utils/encodingUtils.js"
 
+let numberOfRetriesToGetShortVideo = 0 
 const $shortVideoPlayer = document.querySelector('#shortVideoPlayer')
 const $buttonNextShortVideo = document.querySelector('#buttonNextShortVideo')
 
 
 $buttonNextShortVideo.addEventListener('click', async() => {
   $buttonNextShortVideo.disabled = true
-  await nextShorVideo()
+  await nextShortVideo()
   $buttonNextShortVideo.disabled = false
 })
 
@@ -15,16 +16,31 @@ document.querySelector('#buttonPreviousShortVideo').addEventListener('click', ()
   console.log('Previous short video, please!!')
 })
 
-async function nextShorVideo() {
-  const { authorAddress, contentId, metadata, likes } = await getShortVideo()
-  $shortVideoPlayer.src = getUrlFromIpfsHash(getIpfsHashFromBytes32(contentId))
+async function nextShortVideo() {
   try {
+    //Add a case for when there are no videos in the network
+    const { authorAddress, contentId, metadata, likes } = await getShortVideo()
+    $shortVideoPlayer.src = getUrlFromIpfsHash(getIpfsHashFromBytes32(contentId))
     $shortVideoPlayer.load()
-    $shortVideoPlayer.play()
   } catch(error) {
-    console.log(error)
+    console.log("Error when loading the short video", error)
+  }
+  var playPromise = $shortVideoPlayer.play()
+  if (playPromise !== undefined) {
+    playPromise.then(() => {
+      numberOfRetriesToGetShortVideo = 0
+    }).catch((error) => {
+      console.log(`Error when playing the short video. The message is: ${error}. Retry number ${numberOfRetriesToGetShortVideo}`)
+      if(numberOfRetriesToGetShortVideo < 5) {
+        nextShortVideo()
+      }
+      else {
+        console.log('Next short video method was not able to find valid content after all retries')
+      }
+      numberOfRetriesToGetShortVideo++
+    })
   }
 }
 
 //Gets a short video as soon as loading
-nextShorVideo()
+nextShortVideo()
