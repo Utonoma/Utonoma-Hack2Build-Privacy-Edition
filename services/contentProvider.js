75eb8ef6fb99ab97c571047f5878f54c455e09f1
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-05-01 13:03:38 -0600

    [Bugfix]: Changing the method for getting the random numbers in the getShortVideo method. Method was working wrong, as the number obtained where always the same, this error is fixed now

--- services/contentProvider.js
+++ services/contentProvider.js
@@ -3,9 +3,13 @@ import { formatUnits } from 'ethers'
 
 export async function getShortVideo() {
   const { provider, utonomaContract } = useReadOnlyProvider()
-  const shortVideosLibraryLength = formatUnits(await utonomaContract.getContentLibraryLength(5), 0)
-  const identifier = Math.floor(Math.random() * (shortVideosLibraryLength - 1))
-  const { 0: authorAddress, 1: contentId, 2: metadata, 3: likes }  = await utonomaContract.getContentById([identifier,5])
+  try {
+    const shortVideosLibraryLength = formatUnits(await utonomaContract.getContentLibraryLength(5), 0)
+    const identifier = Math.floor(Math.random() * shortVideosLibraryLength)
+    const { 0: authorAddress, 1: contentId, 2: metadata, 3: likes } = await utonomaContract.getContentById([identifier,5])
+  } catch(error) {
+    console.log("Error in getContentLibraryLength or getContentById: ", error)
+  }
 
   return {
     authorAddress,
