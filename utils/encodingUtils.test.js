305b9987f52fb584358761a98cfe1a763fcee648
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-05-01 20:33:27 -0600

    Creating basic unit tests for the getIpfsHashFromBytes32 method

--- utils/encodingUtils.test.js
+++ utils/encodingUtils.test.js
@@ -9,6 +9,38 @@ QmXGk2EonyBiktmXZJNuzWFqe9zvMFyaE6n15JSy9XxEGG
 QmSDWozUsTkRZdA3ksiLrMXRwJupC8vBdk83Ez4UXRp681
 */
 
+const bytes32CidExamples = [
+  '0x7c2f5370a060b0f9a498ef2ebb062c2caeaa3db01d55ebc2b58b66f2ee70e08d',
+  '0xeeba13fef204f6ad2785259d06b0f5ea630e76715f932640e257c55009804cc3'
+]
+
+const convertedBytes32CidExamples = [
+  'QmWhR7sn34AK7Y8MDJhdSGnjzGzzEG1BhxjTpAZb591gRa',
+  'QmeQYJwBo9fh7DafsGCYksRUsQGxEa3gXE7BzqKXd126uY'
+]
+
+test('getIpfsHashFromBytes32 should convert a bytes32 encoded CID into a normal CID V1.0', ()=> {
+  const convertionResult = bytes32CidExamples.map((i) => {
+    return getIpfsHashFromBytes32(i)
+  })
+
+  expect(convertedBytes32CidExamples).toEqual(convertionResult)
+})
+
+test('getIpfsHashFromBytes32 when receiving an empty or null value should throw an exception', () => {
+  const errorMessage = 'no input received'
+
+  const errorReceivingNull = () => {
+    getIpfsHashFromBytes32(null)
+  }
+  const errorReceivingUndefined = () => {
+    getIpfsHashFromBytes32(undefined)
+  }
+
+  expect(errorReceivingNull).toThrow(errorMessage)
+  expect(errorReceivingUndefined).toThrow(errorMessage)
+})
+
 test('getUrlFromIpfsHash should compose a url that points to the provided CID', () => {
   const providedCID = 'QmWhR7sn34AK7Y8MDJhdSGnjzGzzEG1BhxjTpAZb591gRa'
   const actualResult = getUrlFromIpfsHash(providedCID)
