a9e940d19875faa4a98ea529f29c7d3ea1121d66
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-05-03 21:18:16 -0600

    In the shortVideoReel component, separating the state management of the video reel from the presentational part

--- components/ShortVideoReel/ShortVideoReel.js
+++ components/ShortVideoReel/ShortVideoReel.js
@@ -1,9 +1,10 @@
-import { getShortVideo } from "../../services/contentProvider.js"
-import { getUrlFromIpfsHash } from "../../utils/encodingUtils.js"
-
-let shortVideoHistory = []
-let currentVideo = -1;
-let detachedHead = false
+import { getShortVideo } from '../../services/contentProvider.js'
+import { getUrlFromIpfsHash } from '../../utils/encodingUtils.js'
+import {
+  nextShortVideo,
+  getPreviousShortVideo,
+  informCorrectPlay,
+} from './ShortVideoReel.manager.js'
 
 let numberOfRetriesToGetShortVideo = 0 
 const $shortVideoPlayer = document.querySelector('#shortVideoPlayer')
@@ -11,37 +12,12 @@ const $buttonNextShortVideo = document.querySelector('#buttonNextShortVideo')
 const $buttonPreviousShortVideo = document.querySelector('#buttonPreviousShortVideo')
 
 
-$buttonNextShortVideo.addEventListener('click', async() => {
+$buttonNextShortVideo.addEventListener('click', async function thisFunction() {
   $buttonNextShortVideo.disabled = true
   $buttonPreviousShortVideo.disabled = true
-  await nextShortVideo()
-  $buttonNextShortVideo.disabled = false
-  $buttonPreviousShortVideo.disabled = false
-})
 
-document.querySelector('#buttonPreviousShortVideo').addEventListener('click', async() => {
-  $buttonNextShortVideo.disabled = true
-  $buttonPreviousShortVideo.disabled = true
-  await prevShortVideo()
-  $buttonNextShortVideo.disabled = false
-  $buttonPreviousShortVideo.disabled = false
-})
-
-async function nextShortVideo() {
-  let getShortVideoResp
-  if(detachedHead) {
-    updateCurrentShortVideo(currentVideo + 1)
-    getShortVideoResp = shortVideoHistory[currentVideo]
-  }
-  else {
-    //Add a case for when there are no videos in the network
-    getShortVideoResp = await getShortVideo()
-    /*Later you can catch exceptions from getShortVideo with this code 
-    if (Object.keys(myEmptyObj).length === 0) {
-      getShortVideoResp
-    }*/
-  }
-  const { authorAddress, contentId, metadata, likes } = getShortVideoResp
+  const nextShortVideoResp = await nextShortVideo(getShortVideo)
+  const { authorAddress, contentId, metadata, likes } = nextShortVideoResp
   try {
     $shortVideoPlayer.src = getUrlFromIpfsHash(contentId)
     $shortVideoPlayer.load()
@@ -52,29 +28,27 @@ async function nextShortVideo() {
   if (playPromise !== undefined) {
     playPromise.then(() => {
       numberOfRetriesToGetShortVideo = 0
-      if(currentVideo >= shortVideoHistory.length - 1) {
-        //If we are comming out of a detached head state, we are not going to add the video to the history
-        //because we already have it
-        if(detachedHead === false) addShortVideoToHistory(getShortVideoResp)
-        updateDetachedHead(false)
-      }
+      informCorrectPlay(nextShortVideoResp)
     }).catch((error) => {
       console.log(`Error when playing the short video. The message is: ${error}. Retry number ${numberOfRetriesToGetShortVideo}`)
       numberOfRetriesToGetShortVideo++
       if(numberOfRetriesToGetShortVideo < 5) {
-        nextShortVideo()
+        thisFunction(getShortVideo)
       }
       else {
         console.log('Next short video method was not able to find valid content after all retries')
       }
     })
   }
-}
 
-//Gets a short video as soon as loading
-nextShortVideo()
+  $buttonNextShortVideo.disabled = false
+  $buttonPreviousShortVideo.disabled = false
+})
 
-async function prevShortVideo() {
+document.querySelector('#buttonPreviousShortVideo').addEventListener('click', async() => {
+  $buttonNextShortVideo.disabled = true
+  $buttonPreviousShortVideo.disabled = true
+  
   try {
     const { authorAddress, contentId, metadata, likes } = getPreviousShortVideo()
     $shortVideoPlayer.src = getUrlFromIpfsHash(contentId)
@@ -83,25 +57,7 @@ async function prevShortVideo() {
   } catch(error) {
     console.log("Error when loading the previous short video", error)
   }
-}
 
-function addShortVideoToHistory(shortVideoInfo) {
-  shortVideoHistory.push(shortVideoInfo)
-  updateCurrentShortVideo(shortVideoHistory.length - 1)
-}
-
-function updateCurrentShortVideo(index) {
-  currentVideo = index
-}
-
-function updateDetachedHead(boolean) {
-  if (typeof variable == "boolean") throw 'updateDetachedHead only accepts booleans'
-  detachedHead = boolean
-}
-
-function getPreviousShortVideo() {
-  if(currentVideo <= 0 ) throw 'this is the first video, you can not go backwards'
-  updateCurrentShortVideo(currentVideo - 1)
-  updateDetachedHead(true)
-  return shortVideoHistory[currentVideo]
-}
+  $buttonNextShortVideo.disabled = false
+  $buttonPreviousShortVideo.disabled = false
+})
---
+++ components/ShortVideoReel/ShortVideoReel.manager.js
@@ -0,0 +1,49 @@
+let shortVideoHistory = []
+let currentVideo = -1;
+let detachedHead = false
+
+export async function nextShortVideo(apiCall) {
+  console.log(apiCall)
+  if(detachedHead) {
+    updateCurrentShortVideo(currentVideo + 1)
+    return shortVideoHistory[currentVideo]
+  }
+  else {
+    //Add a case for when there are no videos in the network
+    return await apiCall()
+    /*Later you can catch exceptions from getShortVideo with this code 
+    if (Object.keys(myEmptyObj).length === 0) {
+      getShortVideoResp
+    }*/
+    }
+}
+
+export function getPreviousShortVideo() {
+  if(currentVideo <= 0 ) throw 'this is the first video, you can not go backwards'
+  updateCurrentShortVideo(currentVideo - 1)
+  updateDetachedHead(true)
+  return shortVideoHistory[currentVideo]
+}
+
+export function informCorrectPlay(shortVideo) {
+  if(currentVideo >= shortVideoHistory.length - 1) {
+    //If we are comming out of a detached head state, we are not going to add the video to the history
+    //because we already have its
+    if(detachedHead === false) addShortVideoToHistory(shortVideo)
+    updateDetachedHead(false)
+  }
+}
+
+function addShortVideoToHistory(shortVideoInfo) {
+  shortVideoHistory.push(shortVideoInfo)
+  updateCurrentShortVideo(shortVideoHistory.length - 1)
+}
+
+function updateCurrentShortVideo(index) {
+  currentVideo = index
+}
+
+function updateDetachedHead(boolean) {
+  if (typeof variable == "boolean") throw 'updateDetachedHead only accepts booleans'
+  detachedHead = boolean
+}
