9c8dadb3ad3e99fd227eade8ba96aac7b9f8a4f7
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-05-08 04:13:56 -0600

    Basic functionality of the "connect wallet" functionality. New signedProvider file created, will be used for all the operations with the blockchain that require to pay gas.

---
+++ components/ConnectWallet/ConnectWallet.js
@@ -0,0 +1,5 @@
+import {useSignedProvider} from '../../web3_providers/signedProvider.js'
+
+const { modal } = useSignedProvider()
+
+modal.open()
---
+++ web3_providers/signedProvider.js
@@ -0,0 +1,57 @@
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers'
+
+let modal
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = 'acc64a6d2308020280276076ddc6effa'
+
+// 2. Set chains
+const sepolia = {
+  chainId: 11155111,
+  name: 'Sepolia',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia.etherscan.io/',
+  rpcUrl: 'https://sepolia.infura.io/v3/a8b7f3c03367496183ae6e32ad962ee5'
+}
+
+// 3. Create your application's metadata object
+const metadata = {
+  name: 'Utonoma',
+  description: 'The social network of the web3 world',
+  url: 'https://utonoma.com', // url must match your domain & subdomain
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: '...', // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+})
+
+export function useSignedProvider() {
+  if(!modal) {
+    modal = createWeb3Modal({
+      ethersConfig,
+      chains: [sepolia],
+      tokens: {
+        1: { address: '0x1AdaA27C9890c97D605778c2C7B9ff846B8e3352' }
+      },
+      projectId,
+      enableAnalytics: true, // Optional - defaults to your Cloud configuration
+      enableOnramp: true // Optional - false as default
+    })
+  }
+  return {
+    modal,
+    //return an instance of the modal
+    //return the boolean isConnected
+    //return an instance of the contract ready to be used
+  } 
+}
