32418518dd6c1d6e3e9fce622828d757e136beae
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-06-04 00:26:14 -0600

    Confirm dialog before activating account for voting. A set of dialogs where set for the activate account flow, to ask the user for confirmation and to alert it from what the status.

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -7,6 +7,10 @@ import { utonomaSepoliaAddress } from '../../utonomaSmartContract.js'
 import { parseUnits } from 'ethers'
 
 const $settings = document.querySelector('#settings')
+const $modalConfirmActivateForVoting = document.querySelector('#modalConfirmActivateForVoting')
+const $dialogActivateForVotingCheckWallet = document.querySelector('#dialogActivateForVotingCheckWallet')
+const $dialogActivateForVotingTransactionSent = document.querySelector('#dialogActivateForVotingTransactionSent')
+const $dialogActivateForVotingError = document.querySelector('#dialogActivateForVotingError')
 const $connectWallet = document.querySelector('#connectWallet')
 const $buttonActivateForVoting = document.querySelector('#buttonActivateForVoting')
 const $buttonManageAccount = document.querySelector('#buttonManageAccount')
@@ -32,13 +36,34 @@ $buttonManageAccount.addEventListener('click', async () => {
 })
 
 $buttonActivateForVoting.addEventListener('click', async() => {
+  $buttonActivateForVoting.disabled = true
+  $modalConfirmActivateForVoting.showModal()
+  document.querySelector('#buttonConfirmActivateForVotingYes').addEventListener('click', () => {
+    activateForVoting()
+    $modalConfirmActivateForVoting.close()
+    $dialogActivateForVotingCheckWallet.show()
+    setTimeout(() => $dialogActivateForVotingCheckWallet.close(), 5000)
+    $buttonActivateForVoting.disabled = false
+  })
+  document.querySelector('#buttonConfirmActivateForVotingNo').addEventListener('click', () => {
+    $modalConfirmActivateForVoting.close()
+    $buttonActivateForVoting.disabled = false
+  })
+})
+
+async function activateForVoting() {
   try {
     const { utonomaContractForSignedTransactions } = await useUtonomaContractForSignedTransactions()
     const approveResult = await utonomaContractForSignedTransactions.approve(utonomaSepoliaAddress, parseUnits("100000.0", 18))
+    $dialogActivateForVotingTransactionSent.show()
+    setTimeout(() => $dialogActivateForVotingTransactionSent.close(), 5000)
     const transactionResp = await approveResult.wait()
+    //Alert the success
     console.log(transactionResp)
   }
   catch(error) {
     console.log('Error message: ', error)
+    $dialogActivateForVotingError.show()
+    setTimeout(() => $dialogActivateForVotingError.close(), 5000)
   }
-})
+}
