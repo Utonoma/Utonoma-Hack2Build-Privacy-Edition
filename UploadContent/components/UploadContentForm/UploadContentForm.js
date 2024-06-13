571b7d921b3a319a0dedcb1cdf5a8568263cb32e
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-06-13 03:34:26 -0600

    In UploadContent we created a functionality to upload the metadata of the short video to the IPFS network. Created a function to call the Pinata's json pinning service

--- UploadContent/components/UploadContentForm/UploadContentForm.js
+++ UploadContent/components/UploadContentForm/UploadContentForm.js
@@ -1,4 +1,4 @@
-import { shortVideoMetadata } from "../../../services/objectTemplates.js";
+import { ShortVideoMetadata } from '../../../services/models.js';
 
 const $formUploadContent = document.forms['formUploadContent'];
 const [$inputShortVideo, $textAreaShortVideoTitle, $textAreaVideoDescription] = $formUploadContent.elements
@@ -25,7 +25,32 @@ $formUploadContent.addEventListener('submit', async(event) => {
     setTimeout(() => $dialogWrongVideoFileError.close(), 5000)
     return
   }
+
+  //validate that the user's wallet is connected
   
+  //upload metadata to ipfs
+  const metadata = new ShortVideoMetadata()
+  metadata.shortVideoTitle = $textAreaShortVideoTitle.value,
+  metadata.shortVideoDescription = $textAreaVideoDescription.value
+  
+  let metadataHash
+  try {
+    const res = await fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIzYmJlNTYxOS1hM2VmLTRjNzgtYWZjMi04N2E2ZjAzYTg4NTEiLCJlbWFpbCI6ImFkcmlhbi5zZXF1ZWlyYUBvdXRsb29rLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJjMmJjYjVmYTA5YzVhMjY4ODViYSIsInNjb3BlZEtleVNlY3JldCI6ImIyM2ZlYTM0NGJiMTY1Zjg2M2M1ZGQ2NjA4NDExYjFkZTk5OWEwY2Y1MmMwODg0MmRiMzJjNzMyZDljMTg2YmEiLCJpYXQiOjE3MDg2NTMxMjh9.2l4FvQow4eqchALGkxMcdhVTvSjFOxWMtU_ZIVfj2fg', 
+        'Content-Type': 'application/json'
+      },
+      body: `{"pinataContent":${JSON.stringify(metadata)},"pinataMetadata":{"name":"metadata.json"},"pinataOptions":{"cidVersion":0}}`
+    })
+    const jsonResp = await res.json()
+    if(jsonResp.error) throw new Error('Error in the request to upload metadata to IPFS network')
+    metadataHash = jsonResp
+    console.log(jsonResp)
+  } catch (error) {
+    $dialogUploadingDataToIpfsError.show()
+    setTimeout(() => $dialogUploadingDataToIpfsError.close(), 5000)
+  }
 
 });
 
