7e59d4036012e225c019e390bb9bad54e5135966
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-06-25 16:49:21 -0600

    Like content feature created

--- Main/components/ShortVideoReel/ShortVideoReel.js
+++ Main/components/ShortVideoReel/ShortVideoReel.js
@@ -12,6 +12,10 @@ const $buttonNextShortVideo = document.querySelector('#buttonNextShortVideo')
 const $buttonPreviousShortVideo = document.querySelector('#buttonPreviousShortVideo')
 const $buttonLikeShortVideo = document.querySelector('#buttonLikeShortVideo')
 const $likesNumber = document.querySelector('#likesNumber')
+const $dialogActivateForVotingError = document.querySelector('#dialogActivateForVotingError') 
+const $dialogNotEnoughBalanceError = document.querySelector('#dialogNotEnoughBalanceError')
+const $dialogActivateForVotingCheckWallet = document.querySelector('#dialogActivateForVotingCheckWallet')
+const $dialogActivateForVotingTransactionSent = document.querySelector('#dialogActivateForVotingTransactionSent')
 let currentUtonomaIdentifier
 
 $buttonNextShortVideo.addEventListener('click', async() => {
@@ -68,15 +72,91 @@ document.querySelector('#buttonPreviousShortVideo').addEventListener('click', as
     $shortVideoPlayer.play()
     $likesNumber.innerHTML = likes
     currentUtonomaIdentifier = utonomaIdentifier
-    $buttonLikeShortVideo.disabled = true
   } catch(error) {
     console.log("Error when loading the previous short video", error)
   }
 
   $buttonNextShortVideo.disabled = false
   $buttonPreviousShortVideo.disabled = false
+  $buttonLikeShortVideo.disabled = true
 })
 
-$buttonLikeShortVideo.addEventListener('click', () => {
+$buttonLikeShortVideo.addEventListener('click', async() => {
+  $buttonNextShortVideo.disabled = true
+  $buttonPreviousShortVideo.disabled = true
+  $buttonLikeShortVideo.disabled = true
+
   console.log('like button pressed for the content with id: ', currentUtonomaIdentifier)
+  const { useReadOnlyProvider } = await import('../../../web3_providers/readOnlyProvider.js')
+  const { formatUnits } = await import('ethers')
+  const { useUtonomaContractForSignedTransactions, useSignedProvider } = await import('../../../web3_providers/signedProvider.js')
+
+  const { utonomaContract } = useReadOnlyProvider()
+  try {
+    //get the current fee
+    let usersInTheLastPeriod = await utonomaContract.currentPeriodMAU()
+    console.log("current number of users is: ", usersInTheLastPeriod)
+    if(usersInTheLastPeriod == 0) usersInTheLastPeriod = 1
+    const currentFee = await utonomaContract.calculateFee(usersInTheLastPeriod)
+    //console.log('current Fee is: ', formatUnits(currentFee, 18))
+    console.log('current Fee is: ', currentFee)
+
+    //get the allowance of the wallet to the smartcontract
+    const { utonomaContractForSignedTransactions } = await useUtonomaContractForSignedTransactions()
+    const { utonomaSepoliaAddress } = await import('../../../utonomaSmartContract.js') 
+    const { modal } = await useSignedProvider()
+
+    //check that the allowance is higher than the fee
+    const accountAllowance = await utonomaContractForSignedTransactions.allowance(modal.getAddress() , utonomaSepoliaAddress)
+    console.log('allowance from the user to the smartcontract is: ', accountAllowance)
+    if(accountAllowance <= currentFee) throw new Error('Account is not activated')
+
+    //check the balance of utonoma tokens if balance is less than currentFee, then throw
+    const accountBalance = await utonomaContractForSignedTransactions.balanceOf(modal.getAddress())
+    console.log('balance of the account is: ', accountBalance)
+    if(accountBalance <= currentFee) throw new Error('Balance is not enought')
+    
+    //Inform the user what is the fee and ask if accepts
+
+    //Send the transaction to blockchain
+    $dialogActivateForVotingCheckWallet.show()
+    setTimeout(() => $dialogActivateForVotingCheckWallet.close(), 5000)
+    debugger
+    const approveResult = await utonomaContractForSignedTransactions.like([currentUtonomaIdentifier.index, currentUtonomaIdentifier.contentType])
+    $dialogActivateForVotingTransactionSent.show()
+    setTimeout(() => $dialogActivateForVotingTransactionSent.close(), 5000)
+    const transactionResp = await approveResult.wait()
+    //Alert the success
+    console.log(transactionResp)
+
+  } catch(error) {
+    console.log('Error in like short video: ', error)
+
+    //probably is a good idea to do an automated test that checks the real endpoint and see if error.error.message still containing the
+    //expected info
+    if(error.error?.message == 'Please call connect() before request()' || error == 'Error: User disconnected') {
+      console.log('User is not connected')
+      //send the user to the connect wallet screen
+      const { setIsLoggedIn, setAddress } = await import('../../../services/userManager/userManager.js')
+      setIsLoggedIn(false)
+      setAddress('')
+      location.hash = 'rightPanelContainer'
+      setTimeout(() => location.hash = '', 100)
+    }
+    if(error == 'Error: Account is not activated') {
+      $dialogActivateForVotingError.show()
+      setTimeout(() => $dialogActivateForVotingError.close(), 5000)
+      console.log('Account not activated, activate your account in the settings section')
+    }
+
+    if(error == 'Error: Balance is not enought') {
+      console.log('inform the user that it should buy some Utonoma tokens')
+      $dialogNotEnoughBalanceError.show()
+      setTimeout(() => $dialogNotEnoughBalanceError.close(), 5000)
+    }
+  }
+
+  $buttonNextShortVideo.disabled = false
+  $buttonPreviousShortVideo.disabled = false
+  $buttonLikeShortVideo.disabled = false
 })
