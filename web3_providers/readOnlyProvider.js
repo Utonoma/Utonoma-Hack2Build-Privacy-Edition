9c41beedf468d49b0c199ba7f24043d9d3aa02e0
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-07-11 19:40:55 -0600

    jsdocs documentation created for the useReadOnlyProvider method

--- web3_providers/readOnlyProvider.js
+++ web3_providers/readOnlyProvider.js
@@ -5,6 +5,19 @@ import { utonomaSepoliaAddress, utonomaABI} from '../utonomaSmartContract.js'
 let provider
 let utonomaContract
 
+/**
+ * @typedef {Object} useReadOnlyProviderReturn
+ * @property {Object} provider - Handler to the JsonRpcProvider used to create the 
+ * smart contract instance
+ * @property {Object} utonomaContract - The instance of the utonoma contract
+ */
+
+/**
+ * Gets a provider and a smart contract instance for methods that only reads information 
+ * from the blockchain
+ * @returns {useReadOnlyProviderReturn} - {@link useReadOnlyProviderReturn}
+ */
+
 export function useReadOnlyProvider() {
   if(!provider || !utonomaContract) {
     provider = new JsonRpcProvider(sepoliaEndpoint)
