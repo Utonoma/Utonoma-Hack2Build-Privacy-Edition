a8918b325a5d717253028dfc80a0f25fd83b61fa
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-07-15 22:55:40 -0600

    Test cases for the shouldContentBeEliminated method The shouldContentBeEliminated copies the method with the same name from the Utonoma contract, and the test cases are based in the test cases of the same method in the Utonoma smart contract

--- utils/validationUtils/validationUtils.test.js
+++ utils/validationUtils/validationUtils.test.js
@@ -1,4 +1,8 @@
-import { validateVideoDuration } from "./validationUtils.js"
+import { 
+  validateVideoDuration, 
+  shouldContentBeEliminated,
+  canContentBeHarvested 
+} from "./validationUtils.js"
 
 test('validateVideoDuration when receiving a File object in the file parameter should throw an error', async() => {
   const errorMessage = 'invalid file object provided'
@@ -11,3 +15,46 @@ test('validateVideoDuration when receiving a File object in the file parameter s
   ).rejects
   .toThrow(errorMessage)
 })
+
+//shouldContentBeEliminated
+const shouldContentBeEliminatedPositiveCases = [
+  [24, 76],
+  [1, 8],
+  [0, 6],
+  [13, 47],
+  [43310, 98100],
+  [4950000000, 10050000000],
+  [25, 75],
+  [80,107]
+]
+test.each(shouldContentBeEliminatedPositiveCases)(
+  "shouldContentBeEliminated method, when receiving %p likes and %p dislikes, should return true",
+  (likes, dislikes) => {
+    const result = shouldContentBeEliminated(likes, dislikes)
+    expect(result).toBe(true)
+  }
+)
+
+const shouldContentBeEliminatedNegativeCases = [
+  [6, 1],
+  [15, 1],
+  [10, 8],
+  [20, 5],
+  [420, 280],
+  [8, 0],
+  [44, 56],
+  [9759000000, 5241000000],
+  [80, 106],
+  ['a string', 'another string'],
+  [0,0],
+  [-1,0],
+  [-5,-45]
+]
+test.each(shouldContentBeEliminatedNegativeCases)(
+  "shouldContentBeEliminated method, when receiving %p likes and %p dislikes, should return false",
+  (likes, dislikes) => {
+    const result = shouldContentBeEliminated(likes, dislikes)
+    expect(result).toBe(false)
+  }
+)
+
