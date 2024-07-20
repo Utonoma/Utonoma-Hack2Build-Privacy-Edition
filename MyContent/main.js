18584c417cf3a7430ca8e6321113dafe3cb708a9
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-07-19 23:22:53 -0600

    Harvest “Like” functionality has been created. Api call to harvest likes method and alert dialog to confirm transaction and error.

--- MyContent/main.js
+++ MyContent/main.js
@@ -2,6 +2,7 @@ import '../utonoma_styles_library/index.css'
 import { useReadOnlyProvider } from "../web3_providers/readOnlyProvider.js"
 import { getUserAddress } from "../services/userManager/userManager.js"
 import { getIpfsHashFromBytes32 } from "../utils/encodingUtils/encodingUtils.js"
+import { canContentBeHarvested } from '../utils/validationUtils/validationUtils.js'
 
 const { utonomaContract } = useReadOnlyProvider()
 
@@ -46,7 +47,8 @@ async function getContent() {
           1: contentIdInBytes32, 
           2: metadataHashInBytes32, 
           3: likes,
-          4: dislikes
+          4: dislikes,
+          5: harvestedLikes
         } = await utonomaContract.getContentById([identifierIndex, identifierContentType])
   
         const metadata = await fetch(
@@ -54,20 +56,24 @@ async function getContent() {
         )
         const readableMetadata = await metadata.json()
 
+        const isHarvestable = canContentBeHarvested(Number(likes), Number(dislikes), Number(harvestedLikes))
+
         return {
           shortVideoTitle : readableMetadata.shortVideoTitle,
           likes,
-          dislikes
+          dislikes,
+          harvestedLikes,
+          isHarvestable,
+          identifierIndex,
+          identifierContentType
         }
       })
     )
 
     contents.forEach(e => {
-      const $card = $contentInfoCardTemplate.content.cloneNode(true)
-      $card.querySelector('#contentInfoCardTitle').innerText = e.shortVideoTitle
-      $card.querySelector('#contentInfoCardLikes').innerText = e.likes
-      $card.querySelector('#contentInfoCardDislikes').innerText = e.dislikes
-      $tempFragment.appendChild($card)
+      const $template = $contentInfoCardTemplate.content.cloneNode(true)
+      const $contentCard = contentInformationCard($template, e)
+      $tempFragment.appendChild($contentCard)
     })
 
     $cardsContainer.appendChild($tempFragment)
@@ -84,3 +90,67 @@ async function getContent() {
 }
 
 getContent()
+
+const contentInformationCard = (
+  $template, 
+  {
+    identifierIndex,
+    identifierContentType,
+    shortVideoTitle,
+    likes,
+    dislikes,
+    isHarvestable
+  }
+) => {
+  $template.querySelector('#contentInfoCardTitle').innerText = shortVideoTitle
+  $template.querySelector('#contentInfoCardLikes').innerText = likes
+  $template.querySelector('#contentInfoCardDislikes').innerText = dislikes
+  $template.querySelector('.Card__container').setAttribute('data-utonomaId', [identifierIndex, identifierContentType])
+  if(isHarvestable) {
+    $template.querySelector('.Card__container').classList.add('Card__container--glow')
+    $template.querySelector('.Card__actionButton').addEventListener('click', async(e) => {
+      const $container = e.target.closest('.Card__container')
+      try {
+        const { useUtonomaContractForSignedTransactions } = await import('../web3_providers/signedProvider.js')
+        const { utonomaContractForSignedTransactions } = await useUtonomaContractForSignedTransactions()
+        alertCashRewardRequest($container)
+        const harvestLikesReq = await utonomaContractForSignedTransactions.harvestLikes([identifierIndex, identifierContentType])
+        alertCashRewardSent($container)
+        const harvestLikesResp = await harvestLikesReq.wait()
+        console.log(harvestLikesResp)
+      } catch (error) {
+        console.log(error)
+        alertUserNotLoggedIn($container)
+      }
+
+      $container.classList.remove('Card__container--glow')
+      $container.querySelector('.Card__actionButton').style.display = 'none'
+    })
+  }
+  else {
+    $template.querySelector('.Card__actionButton').style.display = 'none' //remove button if content is not harvestable
+  }
+
+  const alertCashRewardRequest = ($container) => {
+    const $approveDialog = $container.querySelector('#dialogCashRewardRequest')
+    $approveDialog.show()
+    setTimeout(() => $approveDialog.close(), 5000)
+  }
+
+  const alertCashRewardSent = ($container) => {
+    const $sentDialog = $container.querySelector('#dialogCashRewardSent')
+    $sentDialog.show()
+    setTimeout(() => $sentDialog.close(), 5000)
+  }
+
+  const alertUserNotLoggedIn = ($container) => {
+    const $errorDialog = $container.querySelector('#dialogCashRewardError')
+    $errorDialog.show()
+    setTimeout(() => { 
+      $errorDialog.close() 
+      window.location.replace('/#rightPanelContainer')
+    }, 8000)
+  }
+
+  return $template
+}
