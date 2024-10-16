7ed5c1dc274b16ebc8d9f9f429bcc1d8b942d4f7
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-10-15 20:25:33 -0600

    Created "I'm a new user" button in the ConnectWallet section. Helps to onboard new users that have no clue on what is a crypto wallet

--- Main/components/ConnectWallet/ConnectWallet.js
+++ Main/components/ConnectWallet/ConnectWallet.js
@@ -5,6 +5,7 @@ import {
   setAddress 
 } from '../../../services/userManager/userManager.js'
 import { createStateForConnectWallet } from "./ConnectWallet.state.js"
+import { useSignedProvider } from '../../../web3_providers/signedProvider.js'
 
 const $settings = document.querySelector('#settings')
 const $connectWallet = document.querySelector('#connectWallet')
@@ -12,10 +13,17 @@ const $connectWallet = document.querySelector('#connectWallet')
 export const ConnectWallet = ($container) => {
   const state = createStateForConnectWallet()
   const $buttonConnectWallet = $container.querySelector('#buttonConnectWallet')
+  const $buttonImANewUser = document.querySelector('#buttonImANewUser')
+
+  $buttonImANewUser.addEventListener('click', async() => {
+    $buttonImANewUser.disabled = true
+    const { modal } = await useSignedProvider()
+    await modal.open({ view: 'WhatIsAWallet' })
+    $buttonImANewUser.disabled = false
+  })
 
   async function effectIsButtonConnectWalletEnabled() {
     $buttonConnectWallet.disabled = true
-    const { useSignedProvider } = await import('../../../web3_providers/signedProvider.js')
     const { modal } = await useSignedProvider()
     modal.subscribeState(async(newState) => {
       if(newState?.open === false) {
