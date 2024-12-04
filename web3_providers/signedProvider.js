0c4a7733cbe4ebf0cd02a9053def1fc8d54fb0df
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-12-03 23:22:33 -0600

    Refactoring the sign in modal to use AppKit instead of WalletConnect

--- web3_providers/signedProvider.js
+++ web3_providers/signedProvider.js
@@ -1,57 +1,37 @@
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers'
+import { createAppKit } from '@reown/appkit'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { scrollSepolia } from '@reown/appkit/networks'
 import { BrowserProvider, Contract } from 'ethers'
 import { utonomaSepoliaAddress, utonomaABI } from '../utonomaSmartContract.js'
 import { sepoliaEndpoint } from './rpcEndpoints.js'
 
 let modal
 
-// 1. Get projectId at https://cloud.walletconnect.com
-const projectId = 'acc64a6d2308020280276076ddc6effa'
+const projectId = '2897ca765c95a7e36410d31f88a6efee' //Id for the demo project
 
-// 2. Set chains
-const sepolia = {
-  chainId: 534351,
-  name: 'Scroll Sepolia Testnet',
-  currency: 'ETH',
-  explorerUrl: 'https://sepolia.scrollscan.com/',
-  rpcUrl: sepoliaEndpoint
-}
-
-// 3. Create your application's metadata object
 const metadata = {
-  name: 'Utonoma',
-  description: 'The social network of the web3 world',
-  url: 'https://utonoma.com', // url must match your domain & subdomain
-  icons: ['https://avatars.mywebsite.com/']
+  name: 'Demo Utonoma',
+  description: 'The demo version of social network of the web3 world',
+  url: 'https://demo.utonoma.com/', // url must match your domain & subdomain
+  icons: ['https://demo.utonoma.com/'] //set an icon
 }
-
-// 4. Create Ethers config
-const ethersConfig = defaultConfig({
-  /*Required*/
-  metadata,
-
-  /*Optional*/
-  enableEIP6963: true, // true by default
-  enableInjected: true, // true by default
-  enableCoinbase: true, // true by default
-  rpcUrl: '...', // used for the Coinbase SDK
-  defaultChainId: 1, // used for the Coinbase SDK
-})
 
 export async function useSignedProvider() {
   return new Promise((resolve) => {
     //Give two seconds before returning the modal, as there are wrong lectures on the getIsConnected method when
     //we return it immediately
     if(!modal) {
-      modal = createWeb3Modal({
-        ethersConfig,
-        chains: [sepolia],
-        tokens: {
-          534351 : { address:  utonomaSepoliaAddress}
-        },
+      modal = createAppKit({
+        adapters: [new EthersAdapter()],
+        networks: [scrollSepolia],
+        metadata,
         projectId,
-        enableAnalytics: true, // Optional - defaults to your Cloud configuration
-        enableOnramp: true // Optional - false as default
+        features: {
+          analytics: true // Optional - defaults to your Cloud configuration
+        },
+        featuredWalletIds: [ //Wallets shown by default when opening the modal
+          '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0' //Trust Wallet
+        ]
       })
     }
     setTimeout(() => {
@@ -61,11 +41,13 @@ export async function useSignedProvider() {
 }
 
 export async function useUtonomaContractForSignedTransactions() {
+  /*
   let { modal } = await useSignedProvider()
   if(!modal.getIsConnected()) throw(new Error('User disconnected'))
   const walletProvider = modal.getWalletProvider()
   const ethersProvider = new BrowserProvider(walletProvider)
   const signer = await ethersProvider.getSigner()
-  const utonomaContractForSignedTransactions = new Contract(utonomaSepoliaAddress, utonomaABI, signer)
+  const utonomaContractForSignedTransactions = new Contract(utonomaSepoliaAddress, utonomaABI, signer)*/
+  const utonomaContractForSignedTransactions = new Object()
   return { utonomaContractForSignedTransactions }
 }
