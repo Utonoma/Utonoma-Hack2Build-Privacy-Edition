27abe62aa3563f34f0c3a861f20cc517c932ea98
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2025-07-07 09:57:40 -0600

    Supporting the delete content functionality in the LikeButton and DislikeButton components. Modified the LikeButton component to support the delete content by creating the alertDelete action. Then, in the DislikeButton component we overload the LikeButton to implement this action, also to overload the watingForApproveOnWallet method to delete instead of dislike depending on the status of the content

--- components/DislikeButton/DislikeButton.js
+++ components/DislikeButton/DislikeButton.js
@@ -1,11 +1,27 @@
 import {ACTIONS, LikeButton } from '../LikeButton/LikeButton.js'
 import { useUtonomaContractForSignedTransactions } from '../../web3_providers/signedProvider.js'
+import { GenericModal as GenericModalFactory } from '../modals/GenericModal/GenericModal.js'
 
 export const DislikeButton = ($container) => {
-  const DislikeButton = LikeButton($container)
+  
   const $dialogCheckWalletToApprove = document.querySelector('#dialogCheckWalletToApprove')
+  
+  const DislikeButton = LikeButton($container)
+  const DialogConfirmDelete = GenericModalFactory(document.querySelector('#dialogConfirmDelete'))
+  
 
+  ACTIONS.alertDelete = 'alertDelete',
+  ACTIONS.waitingForDeletionApproveOnWallet = 'waitingForDeletionApproveOnWallet'
 
+  DislikeButton.actions.alertDelete = async() => {
+    try {
+      await DialogConfirmDelete.actions.showDialog()
+      ? DislikeButton.currentAction = { value: ACTIONS.waitingForDeletionApproveOnWallet }
+      : DislikeButton.currentAction = { value: ACTIONS.waiting }
+    } catch (error) {
+      state.currentAction = { value: ACTIONS.genericError }
+    }
+  }
   DislikeButton.actions.waitingForApproveOnWallet = async() => {
     try {
       $dialogCheckWalletToApprove.showModal()
@@ -13,13 +29,28 @@ export const DislikeButton = ($container) => {
       let likeResult = await utonomaContractForSignedTransactions.dislike([DislikeButton.utonomaIdentifier.index, DislikeButton.utonomaIdentifier.contentType])
       DislikeButton.currentAction = { 
         value: ACTIONS.waitingForBlockchainResult, 
-        payload: likeResult 
-      }
+        payload: likeResult
+      } 
     } catch(error) {
-      DislikeButton.currentAction = { value: ACTIONS.genericError }
+      console.log('Error while disliking content:', error)
     } finally {
       $dialogCheckWalletToApprove.close()
-    } 
+    }
+  } 
+  DislikeButton.actions.waitingForDeletionApproveOnWallet = async() => {
+    try {
+      const { utonomaContractForSignedTransactions } = await useUtonomaContractForSignedTransactions()
+      const transaction = await utonomaContractForSignedTransactions.deletion(DislikeButton.utonomaIdentifier)
+      const likeResult = await transaction.wait()
+      if(likeResult.status === 1) {
+        DislikeButton.currentAction = { value: ACTIONS.deletionSuccess }
+      } else {
+        DislikeButton.currentAction = { value: ACTIONS.deletionError }
+      }
+    } catch (error) {
+      console.error('Error while deleting content:', error)
+      DislikeButton.currentAction = { value: ACTIONS.genericError }
+    }
   }
 
   return DislikeButton
